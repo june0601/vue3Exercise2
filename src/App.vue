f76c9afbe87ae7c80256fc19248c0194fdc05ec5
hope <hope@T0DAA029>
2021-04-22 15:36:11 +0800

    vue3 Exercise 2

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="歡迎來到菜鳥教程！" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
